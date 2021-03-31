451365f1cab117f3541eb9415337ad50b7c6ad57
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2021-03-31 15:34:28 -0600

    final

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ServeStaticModule} from '@nestjs/serve-static';
+import { join } from 'node:path';
 
 
 @Module({
   
   imports: [
+    ServeStaticModule.forRoot({
+      renderPath: join(__dirname, '..', 'avatars')
+    }),
     TypeOrmModule.forRoot(),
     UsuariosModule,
     CategoriesModule
